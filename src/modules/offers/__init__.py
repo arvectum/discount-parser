e4a836ecbc79d26693c920a23682d78c0a5ff421
arutyunov.eth <114887931+arutyunoveth@users.noreply.github.com>
2026-08-08 20:59:38 +0300

    feat(r2): expose offer domain package

---
+++ src/modules/offers/__init__.py
@@ -0,0 +1,27 @@
+from src.modules.offers.models import (
+    ClassificationRule,
+    ManualOverride,
+    Offer,
+    OfferSourceObservation,
+    OfferStatus,
+    OfferType,
+    ParseRun,
+    Publication,
+    PublishFilter,
+    Source,
+)
+from src.modules.offers.repository import OfferRepository
+
+__all__ = [
+    "ClassificationRule",
+    "ManualOverride",
+    "Offer",
+    "OfferRepository",
+    "OfferSourceObservation",
+    "OfferStatus",
+    "OfferType",
+    "ParseRun",
+    "Publication",
+    "PublishFilter",
+    "Source",
+]
