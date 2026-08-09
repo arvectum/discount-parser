4e336c88bed5992f60770b7cd5e82de5646f37ad
arutyunov.eth <114887931+arutyunoveth@users.noreply.github.com>
2026-08-09 08:06:34 +0300

    build: include onboarding router in macOS bundle

--- scripts/build_macos.sh
+++ scripts/build_macos.sh
@@ -14,6 +14,7 @@ pyinstaller --noconfirm --clean --onedir \
   --hidden-import src.web.application \
   --hidden-import src.web.management_pages \
   --hidden-import src.web.system_routes \
+  --hidden-import src.web.onboarding_routes \
   --hidden-import src.web.source_registry_routes \
   --hidden-import src.web.source_registry_static_routes \
   --collect-submodules src.modules.source_registry \
